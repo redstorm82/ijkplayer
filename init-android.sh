ff9d4ef7dfb78277cf49143662633f306b62061e
felix <redstorm82@qq.com>
2019-08-09 14:31:00 +0800

    we upgrade IJK_FFMPEG_COMMIT to ff4.0--ijk0.8.25--20190711--001

--- init-android.sh
+++ init-android.sh
@@ -19,7 +19,8 @@
 # IJK_FFMPEG_UPSTREAM=git://git.videolan.org/ffmpeg.git
 IJK_FFMPEG_UPSTREAM=https://github.com/Bilibili/FFmpeg.git
 IJK_FFMPEG_FORK=https://github.com/Bilibili/FFmpeg.git
-IJK_FFMPEG_COMMIT=ff3.4--ijk0.8.7--20180103--001
+IJK_FFMPEG_COMMIT=ff4.0--ijk0.8.25--20190711--001
+#IJK_FFMPEG_COMMIT=ff3.4--ijk0.8.7--20180103--001
 IJK_FFMPEG_LOCAL_REPO=extra/ffmpeg
 
 set -e
